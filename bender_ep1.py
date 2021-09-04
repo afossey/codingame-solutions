8fd48b3d3556d0eacf9828751d6c32373ec7a234
Antoine FOSSEY <antoine_fossey@carrefour.com>
2021-09-04 16:56:13 +0200

    first commit

---
+++ bender_ep1.py
@@ -0,0 +1,118 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l, c = [int(i) for i in input().split()]
+_map = []
+for i in range(l):
+    row = input()
+    _map.append(list(row))
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+DIRECTION_CODES = {"S": "SOUTH", "E": "EAST", "W": "WEST", "N": "NORTH"}
+SOUTH, EAST, WEST, NORTH = ["SOUTH", "EAST", "WEST", "NORTH"]
+
+def init():
+    teleporters = []
+    suicide_booth = start_pos = None
+    for i, row in enumerate(_map):
+        for j, zone in enumerate(row):
+            if zone == "@":
+                start_pos = [i,j]
+            elif zone == "$":
+                suicide_booth = [i,j]
+            elif zone == "T":
+                teleporters.append([i, j])
+    return [start_pos, teleporters, suicide_booth]
+
+# rules
+def zone(x,y):
+    return _map[x][y]
+
+def next_pos(direction, x, y):
+    if direction == NORTH:
+        return [x-1, y]
+    elif direction == SOUTH:
+        return [x+1, y]
+    elif direction == EAST:
+        return [x, y+1]
+    elif direction == WEST:
+        return [x, y-1]
+
+def is_breakable_obstacle(zone):
+    return zone == "X"
+def is_obstacle(zone):
+    return zone == "#" or is_breakable_obstacle(zone)
+
+def grouped(iterable, n):
+    return zip(*[iter(iterable)]*n)
+
+def avoid_obstacle(direction, x, y):
+    _x, _y = next_pos(direction, x, y)
+    if not is_obstacle(zone(_x, _y)):
+        return direction
+    for obs_dir in obs_dir_priorities:
+        if obs_dir != direction:
+            _x, _y = next_pos(obs_dir, x, y)
+            if not is_obstacle(zone(_x, _y)):
+                return obs_dir
+    raise ValueError("found no direction to avoid obstacle. " + str(direction))
+
+def is_path_modifier(zone):
+    return zone in ["S", "E", "W", "N"]
+def is_circuit_inverter(zone):
+    return zone == "I"
+def is_beer(zone):
+    return zone == "B"
+def is_teleporter(zone):
+    return zone == "T"
+        
+        
+start_pos, teleporters, suicide_booth = init()
+x,y = start_pos
+direction = SOUTH
+obs_dir_priorities = [SOUTH, EAST, NORTH, WEST]
+rampage_mode = False
+output_seq = ""
+output = []
+
+while(True):
+    # suicide booth
+    if zone(x,y) == "$":
+        break
+    if not suicide_booth or len(output_seq) > 2*l*c and output_seq.count(output_seq[2*l*c:3*l*c])>1:
+        output_seq = "LOOP"
+        break
+
+    # check current zone
+    if is_path_modifier(zone(x,y)):
+        direction = DIRECTION_CODES[zone(x,y)]
+    elif is_circuit_inverter(zone(x,y)):
+        obs_dir_priorities.reverse()
+    elif is_beer(zone(x,y)):
+        rampage_mode = not rampage_mode
+    elif is_teleporter(zone(x,y)):
+        x,y = next(filter(lambda tp: tp != [x,y], teleporters))
+    
+    # check beer mode
+    _x,_y = next_pos(direction, x, y)
+    if rampage_mode and is_breakable_obstacle(zone(_x, _y)):
+        _map[_x][_y] = " "
+
+    # go forward avoiding obstacles
+    direction = avoid_obstacle(direction, x, y)
+    x,y = next_pos(direction, x, y)
+    output_seq += direction # seq for infinite loop detection
+    output.append(direction) # list for printing
+
+if output_seq == "LOOP":
+    print("LOOP")
+else: 
+    for d in output:
+        print(d)
+
+
+
---
+++ mars_lander_ep2.py
@@ -0,0 +1,114 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+surface = []
+surface_n = int(input())  # the number of points used to draw the surface of Mars.
+for i in range(surface_n):
+    # land_x: X coordinate of a surface point. (0 to 6999)
+    # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+    land_x, land_y = [int(j) for j in input().split()]
+    surface.append([land_x, land_y])
+
+def get_flat_coords():
+    result = []
+    for i in range(len(surface)):
+        if (i+1 < len(surface)):
+            if surface[i][1]==surface[i+1][1]:
+                result.append(surface[i])
+                result.append(surface[i+1])
+    return result
+
+def distance(a, b=[0,0]):
+    return math.sqrt(math.pow(b[0]-a[0], 2) + math.pow(b[1]-a[1],2))
+
+def closest_point(current, points):
+    dist = -1
+    result = []
+    for point in points:
+        point_dist = distance(current, point)
+        if dist < 0 or point_dist <= dist:
+            dist = point_dist
+            result = point
+    return result
+
+def sum_vectors(vectors):
+    x=y=0
+    for vector in vectors:
+        magn_vect=vector[0]
+        angle_vect=vector[1]
+        x += magn_vect * round(math.cos(math.radians(angle_vect)), 3)
+        y += magn_vect * round(math.sin(math.radians(angle_vect)), 3)
+    return [x, y]
+
+def flight_vector(h_speed, v_speed, r, p):
+    vertical_speed = [v_speed, 90]
+    horizontal_speed = [h_speed, 0]
+    gravity = [-3.711, 90]
+    thrust = [p, 90 + r]
+    return sum_vectors([vertical_speed, horizontal_speed, gravity, thrust]) 
+
+def possible_flight_vectors(h_speed, v_speed, rotate, power): 
+    result = []
+    for p in range(max(power-1, 0), min(power+2, 5)):
+        for r in range(max(rotate-15, -90), min(rotate+16, 91)):
+            v = flight_vector(h_speed, v_speed, r, p)
+            v += [p, r]
+            result.append(v)
+    return result
+
+def travel_safe(vectors, current, target):
+    result = []
+    is_up = current[1] - target[1] < 0
+    is_right = current[0] - target[0] < 0
+    x=y=0
+    if not flat_ground_below(current):
+        x = 60 if is_right else -60
+        y = 15 if is_up else -10
+    optimal_v = [x,y]
+    
+    no_optimal_dist = True
+    d = distance(vectors[0], optimal_v)
+    for vector in vectors:
+        if distance(vector, optimal_v) < d:
+            no_optimal_dist = False
+            break
+    if no_optimal_dist:
+        print("cannot travel safely, rotating..", file=sys.stderr, flush=True)
+        return [0,0,0,0]
+
+    for vector in vectors:
+        if len(result) == 0:
+            result = vector
+        v_dist = distance(vector, optimal_v)
+        r_dist = distance(result, optimal_v)
+        if v_dist < r_dist:
+            result = vector
+    return result
+
+def flat_ground_below(current): 
+    return current[0] > flat_coords[0][0] and current[0] < flat_coords[1][0]
+
+init_height = 0
+flat_coords = get_flat_coords()
+
+# game loop
+while True:
+    # h_speed: the horizontal speed (in m/s), can be negative.
+    # v_speed: the vertical speed (in m/s), can be negative.
+    # fuel: the quantity of remaining fuel in liters.
+    # rotate: the rotation angle in degrees (-90 to 90).
+    # power: the thrust power (0 to 4).
+    x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]
+    if init_height == 0:
+        init_height = y 
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    current = [x,y]
+    landing_point = closest_point(current, flat_coords)
+    descent_point = [landing_point[0], init_height]
+    vectors = possible_flight_vectors(h_speed, v_speed, rotate, power)
+    v = travel_safe(vectors, current, descent_point)
+    print(str(v[3]) + " " + str(v[2]))
+
